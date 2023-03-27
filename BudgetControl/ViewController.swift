c7c220fd1e64ca954876d9c9de86b12ad6e892ef
Yen Lin <e19970722@gmail.com>
2023-03-27 09:14:34 +0800

    Initial Commit

---
+++ BudgetControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetControl
+//
+//  Created by Yen Lin on 2023/3/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
